58aa265c7f556b18f26bbbfb9dcec5da325aa782
Gokul-24-2001 <gokulashok1002@gmail.com>
2025-07-16 22:51:58 +0530

    Create Reversing_words_in_para.js

---
+++ Reversing_words_in_para.js
@@ -0,0 +1,29 @@
+let a="hello world"
+// let s=a.split('');
+let s1=[]
+let rev=""
+
+for(let i=0;i<a.length;i++){
+let ch=a[i]
+if(ch!==" "){
+  rev+=ch
+}
+else{
+  s1.push(rev)
+  rev="";
+}
+
+
+}
+if(rev){
+  s1.push(rev)
+}
+let m=s1.map((word)=>{
+   
+  let r=""
+ for(let i=word.length-1;i>=0;i--){
+  r+=word[i]
+}
+return r})
+
+console.log(rev,s1,m.join(" "));
